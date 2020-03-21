559b8961c2efe19abaddd46dfafc15fa5710f2da
Lucas <lcmhockey@hotmail.com>
2020-03-21 14:29:24 -0400

    added new db

--- db_final.sql
+++ db_final.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Dec 15, 2019 at 08:18 PM
+-- Generation Time: Mar 21, 2020 at 06:25 PM
 -- Server version: 5.7.26
 -- PHP Version: 7.3.8
 
@@ -60,10 +60,15 @@ CREATE TABLE `tbl_content` (
 --
 
 INSERT INTO `tbl_content` (`ID`, `Headings`, `Images`, `Copy`, `Type`, `Tools`) VALUES
-(1, 'Sportsnet Commercial Build', 'piece1.jpg', 'An in class commercial build. This project focused on replicating a Monday Night Football TV intro.', 1, 'Cinema 4D'),
-(2, 'Music Mixer Application', 'piece2.jpg', 'Mix n Scratch is a one page web application that can produce a variety of different beats and mixes. Just drag, drop, and find your groove.', 2, 'Illustrator HTML CSS JS'),
-(3, 'TRAA Website Rebuild', 'piece3.jpg', 'A total rebrand of the website for the Thames River Anglers Association. Full responsiveness and an all new user experience and functionality.', 3, 'Photoshop Illustrator HTML CSS JS'),
-(4, 'ModernLink Media', 'piece4.jpg', 'This project was for an in-class assignment at Fanshawe College. A mockup site design for a potential marketing agency. The website was three pages, fully responsive, and included design and video content', 4, 'HTML CSS JS');
+(1, 'Sportsnet Commercial Build', 'piece4.jpg', 'An in class commercial build. This project focused on replicating a Monday Night Football TV intro.', 1, 'Cinema 4D'),
+(2, 'Music Mixer Application', 'piece4.jpg', 'Mix n Scratch is a one page web application that can produce a variety of different beats and mixes. Just drag, drop, and find your groove.', 2, 'Illustrator HTML CSS JS'),
+(3, 'TRAA Website Rebuild', 'piece4.jpg', 'A total rebrand of the website for the Thames River Anglers Association. Full responsiveness and an all new user experience and functionality.', 3, 'Photoshop Illustrator HTML CSS JS'),
+(4, 'ModernLink Media', 'piece4.jpg', 'This project was for an in-class assignment at Fanshawe College. A mockup site design for a potential marketing agency. The website was three pages, fully responsive, and included design and video content', 4, 'HTML CSS JS'),
+(5, 'Ontario Summer', 'piece4.jpg', 'ggg', 5, 'ggg'),
+(6, 'Thermo Security', 'piece4.jpg', 'ggg', 6, 'dvdcs'),
+(7, 'Audi Testimonial', 'piece4.jpg', 'ggg', 7, 'ggg'),
+(8, 'FIP Event', 'piece4.jpg', 'ggg', 8, 'ggg'),
+(9, 'Sports Science Video', 'piece4.jpg', 'ggg', 9, 'ggg');
 
 -- --------------------------------------------------------
 
@@ -123,7 +128,7 @@ ALTER TABLE `tbl_categories`
 -- AUTO_INCREMENT for table `tbl_content`
 --
 ALTER TABLE `tbl_content`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `tbl_content_categories`

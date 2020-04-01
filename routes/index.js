4b8548a24d4104ff8b98931fe09a63c2a2ed0781
tvanrys <tvanrys@fanshaweonline.ca>
2020-04-01 16:23:06 -0400

    working

--- routes/index.js
+++ routes/index.js
@@ -12,30 +12,31 @@ const sql = require('../utils/sql');
 // });
 
 router.get('/', (req, res) => {
-  
 
-  let query = "SELECT ID, Images, Features FROM tbl_content";
 
- 
+  let query = "SELECT ID, Images, Featured FROM tbl_content";
+
+
 
   sql.query(query, (err, result) => {
     if (err) { console.log(err); } //somethin done broke
 
-    console.log(result); // this should be the database row
+    //console.log(result); // this should be the database row
 
     //featured items with value of 1 will be passed trough
-    //do i list all images in array?
+
+    //do i list all images in array? -> yes, the filter function will return an array with every
+    // item that matches the condition you specifiy (item.Featured === 1)
+
     //first attempt
-    let featured = results[0].filter(item => item.featured === 1);
-    
-  
-    console.log(featured);
-    
+    let featured = result.filter(item => item.Featured === 1);
 
-    // {data: result, featured: featured}
+    // your Terminal should show you two items that match
+    console.log("featured: ", featured);
 
 
-    res.render("layout",{data:result,featured}); // send that row back to the calling function
+    // hero is the key that holds the new featured array -> the 2 items you filtered
+    res.render("layout", { data: result, hero: featured }); // send that row back to the calling function
   })
 })
 
@@ -51,7 +52,7 @@ router.get('/svgdata/:id', (req, res) => {
     console.log(result); // this should be the database row
 
 
-   res.json(result[0]); // send that row back to the calling function
+    res.json(result[0]); // send that row back to the calling function
   })
 
 });

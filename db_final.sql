03fc1fa7c81a06e30f4785840d652759ca7a2a05
Lucas <lcmhockey@hotmail.com>
2020-04-01 14:56:31 -0400

    updated db

--- db_final.sql
+++ db_final.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Mar 21, 2020 at 06:25 PM
+-- Generation Time: Apr 01, 2020 at 06:54 PM
 -- Server version: 5.7.26
 -- PHP Version: 7.3.8
 
@@ -52,23 +52,24 @@ CREATE TABLE `tbl_content` (
   `Images` varchar(30) NOT NULL,
   `Copy` varchar(300) NOT NULL,
   `Type` int(50) NOT NULL,
-  `Tools` varchar(50) NOT NULL
+  `Tools` varchar(50) NOT NULL,
+  `Featured` int(1) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `tbl_content`
 --
 
-INSERT INTO `tbl_content` (`ID`, `Headings`, `Images`, `Copy`, `Type`, `Tools`) VALUES
-(1, 'Sportsnet Commercial Build', 'piece4.jpg', 'An in class commercial build. This project focused on replicating a Monday Night Football TV intro.', 1, 'Cinema 4D'),
-(2, 'Music Mixer Application', 'piece4.jpg', 'Mix n Scratch is a one page web application that can produce a variety of different beats and mixes. Just drag, drop, and find your groove.', 2, 'Illustrator HTML CSS JS'),
-(3, 'TRAA Website Rebuild', 'piece4.jpg', 'A total rebrand of the website for the Thames River Anglers Association. Full responsiveness and an all new user experience and functionality.', 3, 'Photoshop Illustrator HTML CSS JS'),
-(4, 'ModernLink Media', 'piece4.jpg', 'This project was for an in-class assignment at Fanshawe College. A mockup site design for a potential marketing agency. The website was three pages, fully responsive, and included design and video content', 4, 'HTML CSS JS'),
-(5, 'Ontario Summer', 'piece4.jpg', 'ggg', 5, 'ggg'),
-(6, 'Thermo Security', 'piece4.jpg', 'ggg', 6, 'dvdcs'),
-(7, 'Audi Testimonial', 'piece4.jpg', 'ggg', 7, 'ggg'),
-(8, 'FIP Event', 'piece4.jpg', 'ggg', 8, 'ggg'),
-(9, 'Sports Science Video', 'piece4.jpg', 'ggg', 9, 'ggg');
+INSERT INTO `tbl_content` (`ID`, `Headings`, `Images`, `Copy`, `Type`, `Tools`, `Featured`) VALUES
+(1, 'Sportsnet Commercial Build', 'piece1.jpg', 'An in class commercial build. This project focused on replicating a Monday Night Football TV intro.', 1, 'Cinema 4D', 1),
+(2, 'Music Mixer Application', 'traa.png', 'Mix n Scratch is a one page web application that can produce a variety of different beats and mixes. Just drag, drop, and find your groove.', 2, 'Illustrator HTML CSS JS', 0),
+(3, 'TRAA Website Rebuild', 'audi_new.jpg', 'A full website renovation for the Thames River Anglers Association. Along with the help of one other, I designed, coded, and repurposed content producing a fresh six page website informing users on what the TRAA does and how they can get involved.', 3, 'Photoshop Illustrator HTML CSS JS', 0),
+(4, 'ModernLink Media', 'piece4.jpg', 'This project was for an in-class assignment at Fanshawe College. A mockup site design for a potential marketing agency. The website was three pages, fully responsive, and included design and video content', 4, 'HTML CSS JS', 0),
+(5, 'Ontario Summer', 'ontario_new.jpg', 'Working in a group, we were tasked to create a campaign that promoted and attracted people to visit Ontario in the summer. The campaign consisted of a logo, website, and a promotional video. I took the lead on our vision and directed/created our ‘Visit Ontario’ video.', 5, 'ggg', 0),
+(6, 'Thermo Security', 'morph.png', 'ggg', 6, 'dvdcs', 0),
+(7, 'Audi Testimonial', 'audi_new.jpg', 'ggg', 7, 'ggg', 1),
+(8, 'Workout Morph Video', 'thermo_new.jpg', 'ggg', 8, 'ggg', 0),
+(9, 'Sports Science Video', 'baseball.png', 'ggg', 9, 'ggg', 0);
 
 -- --------------------------------------------------------
 

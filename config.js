de327f0153c43d97421496601aee071a0a43d083
Lucas <lcmhockey@hotmail.com>
2020-04-03 11:18:09 -0400

    heroku config added

--- config.js
+++ config.js
@@ -1,23 +1,23 @@
 
-const config = {
-    host: "localhost",
-    port: 8889, // 3306 for windows/linux or newer macs
-    uname: "root",
-    pword: "root", // blank for windows
-    dbase: "db_final_new"
-}
-
-module.exports = config;
-
 // const config = {
-//     host: "us-cdbr-iron-east-04.cleardb.net",
+//     host: "localhost",
 //     port: 8889, // 3306 for windows/linux or newer macs
-//     uname: "b0c08607a0b11e",
-//     pword: "e19e9071", // blank for windows
-//     dbase: "heroku_0c94e95e94f6955"
+//     uname: "root",
+//     pword: "root", // blank for windows
+//     dbase: "db_final_new"
 // }
 
 // module.exports = config;
+
+const config = {
+    host: "us-cdbr-iron-east-01.cleardb.net",
+    port: 8889, // 3306 for windows/linux or newer macs
+    uname: "b29a7221b6627d",
+    pword: "8a6c124e", // blank for windows
+    dbase: "heroku_644f55c90f8c94c"
+}
+
+module.exports = config;
 
 
 

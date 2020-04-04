e2229be2e030cdd2d7cca8e70683fbd618e220c5
Lucas <lcmhockey@hotmail.com>
2020-04-04 13:01:19 -0400

    final push

--- config.js
+++ config.js
@@ -10,11 +10,11 @@
 // module.exports = config;
 
 const config = {
-    host: "us-cdbr-iron-east-01.cleardb.net",
+    host: "us-cdbr-iron-east-01.cleardb.net ",
     port: 8889, // 3306 for windows/linux or newer macs
-    uname: "bae6bc838b216d",
-    pword: "8ab14680", // blank for windows
-    dbase: "heroku_04ff2e0b4b7f026"
+    uname: "b7d9f01e2ae510",
+    pword: "5419b4c5", // blank for windows
+    dbase: "heroku_d73bcde3bdacbca"
 }
 
 module.exports = config;

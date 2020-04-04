b5b9674ad56e3518e9a9a0938b9b6754238354bb
Lucas <lcmhockey@hotmail.com>
2020-04-04 12:44:39 -0400

    final push, i think

--- config.js
+++ config.js
@@ -1,23 +1,23 @@
 
-const config = {
-    host: "localhost",
-    port: 8889, // 3306 for windows/linux or newer macs
-    uname: "root",
-    pword: "root", // blank for windows
-    dbase: "db_final_new"
-}
-
-module.exports = config;
-
 // const config = {
-//     host: "us-cdbr-iron-east-01.cleardb.net",
+//     host: "localhost",
 //     port: 8889, // 3306 for windows/linux or newer macs
-//     uname: "bae6bc838b216d",
-//     pword: "8ab14680", // blank for windows
-//     dbase: "heroku_04ff2e0b4b7f026"
+//     uname: "root",
+//     pword: "root", // blank for windows
+//     dbase: "db_final_new"
 // }
 
 // module.exports = config;
+
+const config = {
+    host: "us-cdbr-iron-east-01.cleardb.net",
+    port: 8889, // 3306 for windows/linux or newer macs
+    uname: "bae6bc838b216d",
+    pword: "8ab14680", // blank for windows
+    dbase: "heroku_04ff2e0b4b7f026"
+}
+
+module.exports = config;
 
 
 
